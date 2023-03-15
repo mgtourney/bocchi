2ce0be5fdf9de0c17b2701a110d26096801cf899
Checksum <checksum@velvet.moe>
2023-03-15 18:11:08 -0500

    fix: tailwing

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss: {
+      config: './tailwind.config.cjs',
+    },
+    autoprefixer: {
+      grid: true,
+    },
   },
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,ts,svelte}'],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   plugins: [],
 }
