be705f089e25dd0a40648b98bb2a4be5c11b6b7b
Skkeye <skkeye@gmail.com>
2023-03-16 19:16:03 -0400

    Added modview (no websockets)

---
+++ src/routes/modview/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { scenes } from "./data";
+
+  function changeScene(e: Event) {
+    if (e.target === null) return;
+    goto(`/overlay-scenes${e.target.id}`);
+  }
+</script>
+
+<div class="flex bg-gray-800 h-[100vh]">
+  <div class="flex flex-col">
+    {#each scenes as scene}
+      <button
+        on:click={changeScene}
+        id={`${scene.id}`}
+        class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4"
+      >
+        {scene.name}
+      </button>
+    {/each}
+  </div>
+</div>
---
+++ src/routes/modview/data.ts
@@ -0,0 +1,10 @@
+export const scenes = [
+  {
+    name: "Starting Soon",
+    id: "/starting-soon",
+  },
+  {
+    name: "Stream Ending",
+    id: "/stream-ending",
+  },
+]
