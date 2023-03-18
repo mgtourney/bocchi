1149ec8c1035b0033bcc58c9a9ac48e1c1024e3c
Checksum <checksum@velvet.moe>
2023-03-18 00:38:51 -0500

    style: timer countdown animations + automatic redirect (work in progress)

--- src/routes/overlay-scenes/+layout.svelte
+++ src/routes/overlay-scenes/+layout.svelte
@@ -1,30 +1,30 @@
 <section class="h-[100vh]">
-  <div class="fixed top-0 left-0 w-full h-full z-[-2] bg-stone-900" />
-  <video
-    class="object-cover w-full h-full fixed top-0 left-0 z-[-1] filter brightness-50"
-    autoplay
-    loop
-    muted
-    playsinline
-    src="/assets/BGVid.mp4"
-  />
+	<div class="fixed top-0 left-0 w-full h-full z-[-2] bg-stone-900" />
+	<video
+		class="object-cover w-full h-full fixed top-0 left-0 z-[-1] filter brightness-50"
+		autoplay
+		loop
+		muted
+		playsinline
+		src="/assets/BGVid.mp4"
+	/>
 
-  <!-- Use a slot to load in the content for the page -->
-  <slot />
+	<!-- Use a slot to load in the content for the page -->
+	<slot />
 </section>
 
 <style>
-  video {
-    opacity: 0;
-    animation: fadeIn 1s ease-in-out forwards;
-  }
+	video {
+		opacity: 0;
+		animation: fadeIn 1s ease-in-out forwards;
+	}
 
-  @keyframes fadeIn {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
+	@keyframes fadeIn {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
 </style>
--- src/routes/overlay-scenes/starting-soon/+page.svelte
+++ src/routes/overlay-scenes/starting-soon/+page.svelte
@@ -1,22 +1,38 @@
 <script lang="ts">
-    import { tweened } from "svelte/motion";
-    let timer = tweened(20 * 60);
+  import { tweened } from "svelte/motion";
+  let timer = tweened(60);
 
-    // Decrement timer each second
-    setInterval(() => {
-        timer.update((n) => n - 1);
-    }, 1000);
+  // Decrement timer each second
+  let interval = setInterval(() => {
+    timer.update((n) => n - 1);
+  }, 1000);
 
-    // Create the reactive components
-    $: minutes = Math.floor($timer / 60);
-    $: seconds = Math.floor($timer % 60);
+  // Create the reactive components
+  $: minutes = Math.floor($timer / 60);
+  $: seconds = Math.floor($timer % 60);
+
+  // If timer is less than or equal to 0, clear the interval
+  $: if ($timer <= 0) {
+    clearInterval(interval);
+
+    // redirect to the next scene (tbd)
+    window.location.href = "/overlay-scenes/starting-soon";
+  }
 </script>
 
 <div class="flex flex-col justify-center items-center h-[100vh]">
-    <div class="bg-black bg-opacity-70 p-12 rounded-xl text-center text-white">
-        <p class="text-5xl mb-4 font-bold">Starting Soon</p>
-        <p class="text-9xl font-semibold">
-            {minutes}:{seconds < 10 ? "0" : ""}{seconds}
-        </p>
-    </div>
+  <div class="bg-black bg-opacity-70 p-16 rounded-xl text-center text-white">
+    <p class="text-8xl font-bold">Starting Soon</p>
+    <p
+      class="text-[9rem] font-bold
+        {minutes === 0 && seconds <= 15 ? 'text-red-500 animate-pulse' : ''}
+        {minutes === 0 && seconds <= 10 ? 'text-red-500 animate-ping' : ''}
+        {minutes === 0 && seconds === 0 ? 'text-red-800' : ''}
+        "
+    >
+      {minutes < 10 ? "0" + minutes : minutes}:{seconds < 10
+        ? "0" + seconds
+        : seconds}
+    </p>
+  </div>
 </div>
