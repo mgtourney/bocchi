2f4de156ed26855b328fba090f306493389f72e3
Checksum <checksum@velvet.moe>
2023-03-17 23:59:48 -0500

    style: I'm just so quirky
    Prettified the starting-soon scene a little.

--- src/routes/overlay-scenes/+layout.svelte
+++ src/routes/overlay-scenes/+layout.svelte
@@ -1,9 +1,30 @@
 <section class="h-[100vh]">
-  <div>
-    <div
-      class="bg-[url('/assets/BGVid.gif')] fixed bg-fixed top-0 bottom-0 left-0 right-0 -z-10 blur scale-105 bg-centered"
-    />
+  <div class="fixed top-0 left-0 w-full h-full z-[-2] bg-stone-900" />
+  <video
+    class="object-cover w-full h-full fixed top-0 left-0 z-[-1] filter brightness-50"
+    autoplay
+    loop
+    muted
+    playsinline
+    src="/assets/BGVid.mp4"
+  />
 
-    <slot />
-  </div>
+  <!-- Use a slot to load in the content for the page -->
+  <slot />
 </section>
+
+<style>
+  video {
+    opacity: 0;
+    animation: fadeIn 1s ease-in-out forwards;
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+</style>
--- src/routes/overlay-scenes/starting-soon/+page.svelte
+++ src/routes/overlay-scenes/starting-soon/+page.svelte
@@ -1,26 +1,22 @@
 <script lang="ts">
-  import { tweened } from "svelte/motion";
-  let original = 20 * 60; // in seconds
-  let timer = tweened(original);
+    import { tweened } from "svelte/motion";
+    let timer = tweened(20 * 60);
 
-  // Decrement timer each second
-  setInterval(() => {
-    if ($timer > 0) $timer--;
-  }, 1000);
+    // Decrement timer each second
+    setInterval(() => {
+        timer.update((n) => n - 1);
+    }, 1000);
 
-  // Prepend a '0' if the number < 10
-  function pad(n: number) {
-    return (n < 10 ? "0" : "") + n;
-  }
-
-  // Create the reactive components
-  $: minutes = pad(Math.floor($timer / 60));
-  $: seconds = pad(Math.floor($timer - Math.floor($timer / 60) * 60));
+    // Create the reactive components
+    $: minutes = Math.floor($timer / 60);
+    $: seconds = Math.floor($timer % 60);
 </script>
 
 <div class="flex flex-col justify-center items-center h-[100vh]">
-  <div class="font-sans font-black leading-14 text-center text-white">
-    <p class="text-5xl">STREAM STARTING</p>
-    <p class="text-7xl">{minutes}:{seconds}</p>
-  </div>
+    <div class="bg-black bg-opacity-70 p-12 rounded-xl text-center text-white">
+        <p class="text-5xl mb-4 font-bold">Starting Soon</p>
+        <p class="text-9xl font-semibold">
+            {minutes}:{seconds < 10 ? "0" : ""}{seconds}
+        </p>
+    </div>
 </div>
