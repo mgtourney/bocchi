5b7a749ca44115b45f4b4add281f91ac3a413788
Skkeye <38301149+Skkeye@users.noreply.github.com>
2023-03-30 05:32:09 -0400

    feat: relay state, i want to die

--- api/src/lib/RelayState.ts
+++ api/src/lib/RelayState.ts
@@ -2,8 +2,11 @@ import {
   Client,
   Models,
   Packets,
+  PlayerWithScore,
   type TAEvents,
 } from "tournament-assistant-client";
+
+export const GameDifficulty = ["Easy", "Normal", "Hard", "Expert", "Expert+"];
 
 export interface Score {
   points?: number;
@@ -18,17 +21,17 @@ export interface Player {
   guid: string;
   steamid: string;
   name: string;
-  score: Score;
-  team: Team;
-  match: Match;
+  score?: Score;
+  team?: Team;
+  teamMembersGUIDs?: string[];
 }
 
 export interface Team {
   guid: string;
-  players: Map<string, Player>; // playerguid -> Player
+  players?: Map<string, Player>; // playerguid -> Player
   name: string;
-  score: Score;
-  avatar: string;
+  score?: Score;
+  avatar?: string;
 }
 
 export interface Song {
@@ -41,8 +44,9 @@ export interface Song {
 export interface Match {
   guid: string;
   coordinator: string;
-  players: Map<string, Player>; // playerguid -> Player
-  teams: Map<string, Team>; // teamguid -> Team
+  players?: Map<string, Player>; // playerguid -> Player
+  teams?: Map<string, Team>; // teamguid -> Team
+  song?: Song;
 }
 
 export interface LastScene {
@@ -50,13 +54,176 @@ export interface LastScene {
   slug: string;
 }
 
+export interface RTData {
+  playerScores: Map<string, Score>;
+  teamScores: Map<string, Score>;
+}
+
 export class RelayState {
-  selectedMatch?: string;
-  private matches?: Map<string, Match>; // matchguid -> Match
+  selectedMatch?: Match;
+  private matches: Map<string, Match> = new Map<string, Match>(); // matchguid -> Match
+  private rtData?: RTData;
   private lastScene: LastScene = {
     page: "not-live",
     slug: ""
   };
+
+  private addMatchFull(match: Models.Match, TAClient: Client) {
+    if (this.selectedMatch == undefined) return;
+    if (match.has_selected_level && match.has_selected_characteristic) {
+      this.selectedMatch.song = {
+        id: match.selected_level.level_id,
+        name: match.selected_level.name,
+        characteristic: match.selected_characteristic.serialized_name,
+        difficulty: GameDifficulty[match.selected_difficulty]
+      }
+    }
+    this.selectedMatch?.players?.forEach((player: Player) => {
+      let teamID = TAClient.getPlayer(player.guid)?.team.id; // TeamGUID is from TA
+      if(teamID == undefined) return;
+      this.selectedMatch?.teams?.get(teamID)?.players?.set(player.guid, player);
+      if(player.score == undefined) {
+        player.score = {
+          points: 0,
+          score: 0,
+          accuracy: 1,
+          misscount: 0,
+          badcutcount: 0,
+          totalmisscount: 0
+        };
+      }
+    });
+    // TODO: Match.Teams.Avatar
+    this.selectedMatch.teams?.forEach((team: Team) => {
+      if(team.score == undefined) {
+        team.score = {
+          points: 0,
+          score: 0,
+          accuracy: 1,
+          misscount: 0,
+          badcutcount: 0,
+          totalmisscount: 0
+        };
+      }
+      team.players?.forEach((player: Player) => {
+        player.team = team;
+      });
+    });
+    this.selectedMatch.players?.forEach((player: Player) => {
+      if(player.team == undefined) return;
+      let iterMembers = this.selectedMatch?.teams?.get(player.team.guid)?.players?.keys();
+      if(iterMembers == undefined) return;
+      Array.from(iterMembers).forEach((member) => {
+        if(member == player.guid) return;
+        player.teamMembersGUIDs?.push(member);
+      });
+    });
+  }
+
+  private addMatchGeneral(match: Models.Match, TAClient: Client) {
+    // init match primatives
+    let matchPlayers: Map<string, Player> = new Map<string, Player>();
+    let matchTeams: Map<string, Team> = new Map<string, Team>();
+    // add minimal info to match primatives
+    TAClient.getMatchPlayers(match).forEach((player: PlayerWithScore) => {
+      matchTeams.set(player.team.id, {
+        guid: player.team.id,
+        name: player.team.name
+      });
+      matchPlayers.set(player.guid, {
+        guid: player.guid,
+        steamid: player.user_id,
+        name: player.name
+      });
+    });
+    // add match
+    this.matches.set(match.guid, {
+      guid: match.guid,
+      coordinator: match.leader,
+      players: matchPlayers,
+      teams: matchTeams
+    });
+  }
+
+  initMatches(TAClient: Client) {
+    TAClient.State.matches.forEach((match: Models.Match) => {
+      this.addMatchGeneral(match, TAClient);
+    })
+  }
+
+  addMatch(match: Models.Match, TAClient: Client) {
+    this.addMatchGeneral(match, TAClient);
+  }
+
+  updateMatch(match: Models.Match, TAClient: Client) {
+    if (match.guid == this.selectedMatch?.guid) {
+      this.addMatchFull(match, TAClient);
+    } else {
+      this.addMatchGeneral(match, TAClient);
+    }
+  }
+
+  selectMatch(matchGUID: string | undefined, TAClient: Client) {
+    if (matchGUID == undefined) return;
+    let match = TAClient.getMatch(matchGUID);
+    if(match == undefined) return;
+    match.associated_users.push(TAClient.Self.guid);
+    this.selectedMatch = this.matches.get(matchGUID);
+    this.addMatchFull(match, TAClient);
+  }
+
+  deleteMatch(matchGUID: string | undefined) {
+    if (matchGUID == undefined) return;
+    if (this.selectedMatch?.guid == matchGUID) this.selectedMatch = undefined;
+    this.matches.delete(matchGUID);
+  }
+
+  getState() {
+    return {
+      matches: this.matches,
+      match: this.selectedMatch,
+      players: this.selectedMatch?.players,
+      teams: this.selectedMatch?.teams
+    };
+  }
+
+  updateRTScore(realtimeScore: TAEvents.PacketEvent<Packets.Push.RealtimeScore>, callback: CallableFunction) {
+    let teamGUID = this.selectedMatch?.players?.get(realtimeScore.data.user_guid)?.team?.guid;
+    let otherPlayersGUID = this.selectedMatch?.players?.get(realtimeScore.data.user_guid)?.teamMembersGUIDs;
+    if (teamGUID == undefined) return;
+    let updatedPlayer = this.selectedMatch?.players?.get(realtimeScore.data.user_guid);
+    if(updatedPlayer == undefined) return; 
+      updatedPlayer.score = {
+      score: realtimeScore.data.score_with_modifiers,
+      accuracy: realtimeScore.data.accuracy,
+      misscount: realtimeScore.data.scoreTracker.notesMissed,
+      badcutcount: realtimeScore.data.scoreTracker.badCuts,
+      totalmisscount: realtimeScore.data.scoreTracker.notesMissed + realtimeScore.data.scoreTracker.badCuts
+    };
+    let cumulScore = 0, cumulAcc = 0, totalAcc = 0, cumulMiss = 0, cumulBad = 0, cumulTotalMiss = 0;
+    let updatedPlayers = this.selectedMatch?.players;
+    if(updatedPlayers == undefined) return;
+    updatedPlayers.forEach((player) => {
+      if(player.score == undefined) return;
+      if (!otherPlayersGUID?.includes(player.guid)) return;
+      cumulScore += player.score.score;
+      cumulAcc += player.score.accuracy;
+      totalAcc += 1;
+      cumulMiss += player.score.misscount;
+      cumulBad += player.score.badcutcount;
+      cumulTotalMiss += player.score.totalmisscount;
+    });
+    let updatedTeam = this.selectedMatch?.teams?.get(teamGUID);
+    if(updatedTeam == undefined) return;
+    updatedTeam.score = {
+      score: cumulScore,
+      accuracy: Math.round((cumulAcc / totalAcc) * 10000) / 100,
+      misscount: cumulMiss,
+      badcutcount: cumulBad,
+      totalmisscount: cumulTotalMiss
+    };
+    callback(this.rtData);
+  }
 
   getLastScene() {
     return this.lastScene;
@@ -66,24 +233,10 @@ export class RelayState {
     this.lastScene = scene;
   }
 
-  getAllMatches() {
-    return this.matches;
-  }
-
-  getMatch() {
-    if(this.selectedMatch == undefined) return undefined;
-    return this.matches?.get(this.selectedMatch);
-  }
-
-  selectMatch(match: string | undefined) {
-    this.selectedMatch = match;
-  }
-
-  updateState(taClient: Client) {
-
-  }
-
-  updateRTScore(realtimeScore: Packets.Push.RealtimeScore) {
-
+  setTeamPoints(teamGUID: string, points: number) {
+    let teamBackup = this.selectedMatch?.teams?.get(teamGUID);
+    if (teamBackup?.score == undefined) return;
+    teamBackup.score.points = points;
+    this.selectedMatch?.teams?.set(teamGUID, teamBackup);
   }
 }
