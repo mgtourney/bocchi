7460b381a36c7f6032c75af0cfa92b47a31be7eb
Checksum <me@checksum.space>
2023-04-06 16:48:18 -0500

    style: disabled muted transition

--- web/src/routes/scenes/test/+page.svelte
+++ web/src/routes/scenes/test/+page.svelte
@@ -1,1 +1,2 @@
-<video src="/assets/stream_transition.webm" autoplay loop muted></video>
+<!-- svelte-ignore a11y-media-has-caption -->
+<video src="/assets/stream_transition.webm" autoplay loop></video>
