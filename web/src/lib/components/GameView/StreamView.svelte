6392ee094c4c11db1e53d701b828a8edab93f87c
Checksum <me@checksum.space>
2023-04-08 13:47:29 -0500

    fix: please work

--- web/src/lib/components/GameView/StreamView.svelte
+++ web/src/lib/components/GameView/StreamView.svelte
@@ -6,12 +6,20 @@
 
   let width = 1920 * scale;
   let height = 1080 * scale;
+
+  let overlaySrc;
+
+  // Wait a random amount of time before loading the iframe (500ms to 1000)
+  setTimeout(() => {
+    overlaySrc =
+      "https://player.twitch.tv/?channel={twitchName}&height={height}&width={width}&parent=overlay.magnesium.gg&autoplay=true&muted={muted}&controls=false";
+  }, Math.random() * 5000);
 </script>
 
 <iframe
   id="{twitchName}-twitch"
   title="{twitchName}'s stream"
-  src="https://player.twitch.tv/?channel={twitchName}&height={height}&width={width}&parent=overlay.magnesium.gg&autoplay=true&muted={muted}&controls=false"
+  src={overlaySrc}
   {height}
   {width}
   frameborder="0"
