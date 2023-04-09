afafcb57794476601115bc6e596a1939fecc7038
Checksum <me@checksum.space>
2023-04-09 13:16:02 -0500

    style: diffing the diff bar differently

--- web/src/lib/components/DiffBar.svelte
+++ web/src/lib/components/DiffBar.svelte
@@ -3,23 +3,27 @@
   import { tick } from "svelte";
   export let diff: number;
 
-  // Spring only diff so that the bar doesn't shift around if the diff changes rapidly around 0
   let barDiff = spring(0);
   $: barDiff.set(diff);
 
   let barLengthR = 0;
   let barLengthL = 0;
 
-  $: barLengthR = $barDiff >= 0 ? (100 - $barDiff) / 2 : 50;
-  $: barLengthL = $barDiff < 0 ? (100 - Math.abs($barDiff)) / 2 : 50;
-
-  $: tick();
+  $: {
+    if ($barDiff > 0) {
+      barLengthR = Math.min(100, $barDiff * 100);
+      barLengthL = 0;
+    } else {
+      barLengthL = Math.min(100, -$barDiff * 100);
+      barLengthR = 0;
+    }
+  }
 </script>
 
 <div class="flex w-full h-3">
-  <div class="bg-bar" style:width={`${barLengthL * 8}%`} />
+  <div class="bg-bar" style:width={`${barLengthL}%`} />
   <div class="flex-grow {diff < 0 ? 'bg-blue-600' : 'bg-orange-600'}" />
-  <div class="bg-bar" style:width={`${barLengthR * 8}%`} />
+  <div class="bg-bar" style:width={`${barLengthR}%`} />
 </div>
 
 <style>
